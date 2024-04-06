af5db25aa4a085451c541fb0c1613f2dc537be9b
MaxRayden <max.rayden.etech@gmail.com>
2024-04-05 21:36:20 -0300

    codigo de idade aulaC++

---
+++ aula.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float altura;
+    float idade;
+    cout << "Digite sua idade" << endl;
+    cin >> altura;
+    cout << "Digite sua altura" << endl;
+    cin >> idade;
+    cout <<"Sua altura eh:  "<< altura << endl;
+    cout <<"Sua idade eh:  "<< idade << endl;
+    return 0;
+}
