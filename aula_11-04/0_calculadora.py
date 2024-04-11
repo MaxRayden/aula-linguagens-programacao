3ad83a6b595e3f66a866e452d574fb7df4087dbe
MaxRayden <max.rayden.etech@gmail.com>
2024-04-11 18:47:50 -0400

    creat code - 0_calc

---
+++ aula_11-04/0_calculadora.py
@@ -0,0 +1,19 @@
+valor1 = float(input("Digite um número: "))
+
+while valor1 != -1:
+    operador = input("Digite um operador: ")
+    valor2 = float(input("Digite outro número: "))
+    resultado = 0
+    if operador == '+':
+        resultado = valor1 + valor2
+    elif operador == '-':
+        resultado = valor1-valor2
+    elif operador == '*':
+        resultado = valor1*valor2
+    elif operador == '/':
+        resultado = valor1/valor2
+    else:
+        print('operador inválido')
+    print(resultado,"\n\n")
+    valor1 = float(input("Digite um número: "))
+print("encerrando...")
